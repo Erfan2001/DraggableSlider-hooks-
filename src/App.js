0c5f6bc39895dfad3ec845aa7a0de649037ac4c6
Erfan <1380erfan2001@gmail.com>
2020-10-28 11:29:10 +0330

    draggableSlider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState, useRef, useEffect } from "react";
 function App() {
+  const [margin, setMargin] = useState();
+  const [move, setMove] = useState(false);
+
+  let innerRef = useRef();
+
+  useEffect(() => {
+    innerRef.current.style.marginLeft = margin;
+  }, [margin]);
+
+  const mouseDown = (e) => {
+    setMove(true);
+  };
+
+  useEffect(()=>{
+    window.addEventListener("mouseup",()=>{
+      setMove(false)
+    })
+  })
+
+  const test=()=>{
+    if((window.event.screenX >500) && (window.event.screenX <900) && move){
+    setMargin(window.event.screenX - 500 + "px");
+    }
+  }
+  useEffect(()=>{
+      window.addEventListener("mousemove",test) 
+      return ()=>{
+        window.removeEventListener("mousemove",test) 
+      }
+  },[move])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="test"></div>
+      <div className="Out" >
+        <div
+          className="In"
+          onMouseDown={mouseDown}
+          // onMouseUp={mouseup}
+          ref={innerRef}
+          style={{ marginLeft: "0px" }}
+        ></div>
+      </div>
     </div>
   );
 }
-
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import "./App.css";
+// import React, { useState, useRef, useEffect } from "react";
+// function App() {
+//   const [margin, setMargin] = useState();
+//   const [move, setMove] = useState(false);
+//   let innerRef = useRef();
+//   useEffect(() => {
+//     innerRef.current.style.marginLeft = margin;
+//   }, [margin]);
+//   const mouseDown = (e) => {
+//     setMove(true);
+//   };
+//   useEffect(()=>{
+//     const x=window.addEventListener("mousemove",()=>{
+//       if (window.event.screenY < 500 || window.event.screenY > 520 || window.event.screenX<500 || window.event.screenX >1000) {
+//         setMove(false);
+//       }
+//     })
+//     return ()=>{
+//       window.removeEventListener("mousemove",x)
+//     }
+//   },[])
+//   const moved = () => {
+//     if (
+//       (Number(innerRef.current.style.marginLeft.slice(0, -2)) < 400 ||
+//         window.event.screenX < 900) &&
+//       move
+//     ) {
+//       setMargin(window.event.screenX - 500 + "px");
+//     }
+//     console.log(window.event.screenX)
+//   };
+//   const mouseUp = () => {
+//     setMove(false);
+//   };
+//   return (
+//     <div className="App">
+//       <div className="test"></div>
+//       <div className="Out" onMouseMove={moved}>
+//         <div
+//           className="In"
+//           onMouseDown={mouseDown}
+//           onMouseUp={mouseUp}
+//           ref={innerRef}
+//           style={{ marginLeft: "0px" }}
+//         ></div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
